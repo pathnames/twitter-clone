173421339da9bfb65f472e8d1baabfaa5112a757
pjkpjk900 <pk58@buffalo.edu>
2020-06-18 15:11:52 -0400

    Added loading gif to form and hid form on submission

---
+++ client/client.js
@@ -0,0 +1,22 @@
+console.log('Hello World!');
+const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+
+loadingElement.style.display = 'none';
+
+form.addEventListener('submit', (event) => {
+   event.preventDefault();
+   console.log('form submitted');
+   const formData = new FormData(form);
+   const name = formData.get('name');
+   const content = formData.get('content');
+
+   const mew = {
+       name, 
+       content
+   }
+   console.log(mew);
+   form.style.display = 'none';
+   loadingElement.style.display = '';
+
+});
