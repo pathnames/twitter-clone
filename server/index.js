e5b61c71c7c9fdeb79ce0f42bc3226d6b717edef
pjkpjk900 <pk58@buffalo.edu>
2020-06-18 20:00:22 -0400

    Added GET and POST routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Meow! :smile_cat:'
+    });
+});
+
+app.post('/mews', (req, res) => {
+    console.log(req.body);
+});
+
 app.listen(5000, () => {
     console.log("Listening on port 5000!");
 });
