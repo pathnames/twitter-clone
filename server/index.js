968dab64114431274abf6c62454c8949861a0128
pjkpjk900 <pk58@buffalo.edu>
2020-06-18 20:38:27 -0400

    Form validation

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({
@@ -8,8 +12,23 @@ app.get('/', (req, res) => {
     });
 });
 
+function isValidMew(mew){
+    return mew.name && mew.name.toString().trim() !== '' && mew.content && mew.content.toString().trim() !== '';
+}
 app.post('/mews', (req, res) => {
     console.log(req.body);
+    if(isValidMew(req.body)){
+        //insert into db...
+        const mew = {
+            name: req.body.name.toString(),
+            content: req.body.content.toString()
+        }
+     }else{
+        res.status(422);
+        res.json({
+            message: 'Name and content are required!'
+        });
+    }
 });
 
 app.listen(5000, () => {
